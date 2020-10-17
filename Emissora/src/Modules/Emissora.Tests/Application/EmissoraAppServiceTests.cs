d9deced57118a25d5693b11ec5a1f8086a4cce9b
Bruno Melo de Souza <63356312+brunosouza1977@users.noreply.github.com>
2020-10-17 00:48:49 -0300

    Criada a camada de teste e métodos de inserção e retorno

---
+++ Emissora/src/Modules/Emissora.Tests/Application/EmissoraAppServiceTests.cs
@@ -0,0 +1,61 @@
+﻿using Castle.Core.Configuration;
+using Emissora.Aplication.AppEmissora;
+using Emissora.Domain.Entities;
+using Emissora.Domain.Interfaces.Repositories;
+using Emissora.Infrastructure.Repositories;
+using FluentAssertions;
+using Marraia.Notifications;
+using Marraia.Notifications.Interfaces;
+using NSubstitute;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using Xunit;
+
+namespace Emissora.Tests.Application
+{
+    public class EmissoraAppServiceTests
+    {
+        private IUsuarioRepository usuarioRepository;
+        private ISmartNotification smartNotification;
+        private IConfiguration configuration;
+        private UsuarioAppService usuarioAppService;
+
+
+
+        public EmissoraAppServiceTests()
+        {
+            usuarioRepository = Substitute.For<IUsuarioRepository>();
+            smartNotification = Substitute.For<ISmartNotification>();
+
+            usuarioRepository = new UsuarioRepository((Microsoft.Extensions.Configuration.IConfiguration)configuration);
+            usuarioAppService = new UsuarioAppService(smartNotification, usuarioRepository);
+                 
+
+
+        }
+
+        [Fact]
+        public void Validar_retorno_get_com_sucesso()
+        {
+            //arrange
+            var lista = new List<Usuario>();
+            lista.Add(new Usuario(1, "Bruno Melo", 97524492, "bruno", "1234", 1, 200000, 1,DateTime.Today, "Recife", "PE", 53050260, DateTime.Today));
+            usuarioRepository.Get().Returns(lista);
+
+            //act
+            var retorno = usuarioAppService.Get();
+
+
+            //assert
+            retorno.Should().HaveCount(1);
+
+            usuarioRepository.Get().Received(1);
+        }
+            
+
+
+
+    }
+}
